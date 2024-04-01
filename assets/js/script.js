ebab7c0cb3c02104e5018f6c1a643a6594a8f582
mateus124 <alvesmateusq00@gmail.com>
2024-04-01 10:51:17 -0300

    game register

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,21 @@ tabs.forEach(tab =>{
         tab.classList.add('active-button')
     })
 })
+
+function submitForm() {
+    gameData = {Name: document.getElementById('name').value, 
+                Desc: document.getElementById('desc').value,
+                Image: document.getElementById('image-link').value,
+                Genus: document.getElementById('Genus').value,
+                Mode: document.getElementById('mode').value,
+                Trophy: document.getElementById('trophy').value,
+                Plataform: document.getElementById('plataform').value}
+
+    var storedData = localStorage.getItem("gameData");
+    var gamesArray = storedData ? JSON.parse(storedData) : [];
+    gamesArray.push(gameData);
+    localStorage.setItem("gameData", JSON.stringify(gamesArray));
+
+    alert("O jogo foi registrado!");
+    document.getElementById('game-form').reset();
+}
